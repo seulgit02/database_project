8c5223208bd866da93c5d5ebc2cf7e222d375c7d
seulgibaek <seulgi.baek@dgu.ac.kr>
2024-12-01 18:34:24 +0900

    [feat] 프론트 초기세팅 완료

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ["./src/**/*.{html,js, jsx, tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#EECEC1",
+        secondary: "#B1827A",
+        accent: "#5F494E",
+        sky: "#89A8AE",
+        background: "#F5F5F3",
+      },
+    },
+  },
+  plugins: [],
+};
